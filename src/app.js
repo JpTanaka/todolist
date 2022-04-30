4c81fb453ce543abab79d9b652e8963bd5246990
JpTanaka <jpao.com@gmail.com>
2022-04-30 17:47:21 +0200

    Started, added textform

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class TextForm extends React.Component {
+    render () {
+        return (
+            <form>
+                <input type='text' placeholder='What do you have to do?'></input>
+            </form>
+        );
+    }
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(<TextForm />);
+
+
+
