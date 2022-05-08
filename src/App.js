079f88df08c45fa99ce044882075982543a7c0d4
JpTanaka <jpao.com@gmail.com>
2022-05-08 11:30:18 +0200

    took out pre-made cards

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends React.Component {
     super(props);
     this.state = {
     ToDoNumber: 1,
-    TodoList: ["aaaaaaaaaaaaaadfffffffffffffffffffff", "aaaaaaaadffffffffffsddd"],
+    TodoList: [],
     Textvalue: '',
     }
   }
